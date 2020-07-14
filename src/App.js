f116b946c2228725067304a167e5fa0ea9ccd83b
super0605 <36216137+super0605@users.noreply.github.com>
2020-07-14 23:18:07 +0800

    Updated resume

--- src/App.js
+++ src/App.js
@@ -354,7 +354,7 @@ function App() {
                   </p>
                   <p>
                     <a
-                      href="./ZhangWeiResume.pdf"
+                      href="./DonZhangResume.pdf"
                       target="_blank"
                       rel="noopener noreferrer noreferrer"
                       className="btn btn-primary py-3 px-3"
